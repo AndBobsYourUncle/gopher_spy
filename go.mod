5e9ca3f3ea9a5dc4394ed56fcd5ed21f6748a7c6
Nicholas Page <nicholas.page@goat.com>
2023-12-27 09:15:07 -0800

    proto

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.21
 
 require (
 	github.com/bluenviron/gortsplib/v4 v4.6.2
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/nsmith5/mjpeg v0.0.0-20200913181537-54b8ada0e53e
 	github.com/pion/rtp v1.8.3
+	github.com/yalue/onnxruntime_go v1.6.0
+	golang.org/x/image v0.14.0
 )
 
 require (
@@ -14,12 +17,10 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/llgcode/draw2d v0.0.0-20231212091825-f55e0c776b44 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
 	github.com/pion/rtcp v1.2.12 // indirect
 	github.com/pion/sdp/v3 v3.0.6 // indirect
-	github.com/yalue/onnxruntime_go v1.6.0 // indirect
-	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,204 +1,192 @@
 package main
 
 import (
-	"fmt"
-	"github.com/bluenviron/gortsplib/v4"
-	"github.com/bluenviron/gortsplib/v4/pkg/base"
-	"github.com/bluenviron/gortsplib/v4/pkg/format"
-	"github.com/bluenviron/gortsplib/v4/pkg/format/rtph264"
-	"github.com/nsmith5/mjpeg"
-	"github.com/pion/rtp"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/basicfont"
 	"golang.org/x/image/math/fixed"
 	"image"
 	"image/color"
-	"image/jpeg"
-	"log"
-	"net/http"
-	"sync/atomic"
-	"time"
 )
 
 func main() {
-	c := gortsplib.Client{}
-
-	// parse URL
-	u, err := base.ParseURL("rtsp://192.168.1.20:8554/unicast")
-	if err != nil {
-		log.Fatalf("Error parsing URL: %v", err)
-	}
-
-	// connect to the server
-	err = c.Start(u.Scheme, u.Host)
-	if err != nil {
-		log.Fatalf("Error connecting to server: %v", err)
-	}
-	defer c.Close()
-
-	// find available medias
-	desc, _, err := c.Describe(u)
-	if err != nil {
-		log.Fatalf("Error describing stream: %v", err)
-	}
-
-	// find the H264 media and format
-	var forma *format.H264
-	medi := desc.FindFormat(&forma)
-	if medi == nil {
-		log.Fatalf("H264 media not found")
-	}
-
-	// setup RTP/H264 -> H264 decoder
-	rtpDec, err := forma.CreateDecoder()
-	if err != nil {
-		log.Fatalf("Error creating decoder: %v", err)
-	}
-
-	// setup H264 -> raw frames decoder
-	frameDec := &h264Decoder{}
-	frameDec.initialize()
-	if err != nil {
-		log.Fatalf("Error creating decoder: %v", err)
-	}
-	defer frameDec.close()
-
-	// if SPS and PPS are present into the SDP, send them to the decoder
-	if forma.SPS != nil {
-		frameDec.decode(forma.SPS)
-	}
-	if forma.PPS != nil {
-		frameDec.decode(forma.PPS)
-	}
-
-	// setup a single media
-	_, err = c.Setup(desc.BaseURL, medi, 0, 0)
-	if err != nil {
-		log.Fatalf("Error setting up stream: %v", err)
-	}
-
-	imgFrames := make(chan image.Image)
-
-	// called when a RTP packet arrives
-	c.OnPacketRTP(medi, forma, func(pkt *rtp.Packet) {
-		// decode timestamp
-		_, ok := c.PacketPTS(medi, pkt)
-		if !ok {
-			//log.Printf("waiting for timestamp")
-			return
-		}
-
-		// extract access units from RTP packets
-		au, err := rtpDec.Decode(pkt)
-		if err != nil {
-			if err != rtph264.ErrNonStartingPacketAndNoPrevious && err != rtph264.ErrMorePacketsNeeded {
-				log.Printf("ERR: %v", err)
-			}
-			return
-		}
-
-		for _, nalu := range au {
-			// convert NALUs into RGBA frames
-			img, err := frameDec.decode(nalu)
-			if err != nil {
-				log.Fatalf("Error decoding frame: %v", err)
-			}
-
-			// wait for a frame
-			if img == nil {
-				continue
-			}
-
-			imgFrames <- img
-
-			//log.Printf("decoded frame with PTS %v and size %v", pts, img.Bounds().Max)
-		}
-	})
-
-	// start playing
-	_, err = c.Play(nil)
-	if err != nil {
-		log.Fatalf("Error opening stream: %v", err)
-	}
-
-	detectedImg := make(chan image.Image)
-
-	processingImage := atomic.Bool{}
-
-	lastTime := time.Now()
-	fps := 0.0
-
-	latestDetections := []*Detection{}
-
-	go func() {
-		for {
-			img := <-detectedImg
-
-			t1 := time.Now()
-
-			detections, detErr := detect_objects_on_image(img)
-			if detErr != nil {
-				log.Printf("error detecting objects: %v", detErr)
-			}
-
-			for _, detection := range detections {
-				log.Printf("detected: %+v", detection)
-			}
-
-			latestDetections = detections
-
-			detectionFps := 1 / time.Since(t1).Seconds()
-
-			log.Printf("stream fps: %.1f, detection fps: %.1f", fps, detectionFps)
-
-			processingImage.Store(false)
-		}
-	}()
-
-	stream := mjpeg.Handler{
-		Next: func() (image.Image, error) {
-			img := <-imgFrames
-
-			fps = 1 / time.Since(lastTime).Seconds()
-			lastTime = time.Now()
-
-			if !processingImage.Load() {
-				processingImage.Store(true)
-				detectedImg <- img
-			}
-
-			// draw bounding boxes
-			for _, detection := range latestDetections {
-				// draw rectangle
-				drawHollowRectangle(
-					img.(*image.RGBA),
-					int(detection.X1),
-					int(detection.Y1),
-					int(detection.X2-detection.X1),
-					int(detection.Y2-detection.Y1),
-					2,
-				)
-
-				// add label
-				addLabel(
-					img.(*image.RGBA),
-					int(detection.X1),
-					int(detection.Y1),
-					fmt.Sprintf("%s %.2f", detection.Label, detection.Probability),
-				)
-			}
-
-			return img, nil
-		},
-		Options: &jpeg.Options{Quality: 80},
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/stream", stream)
-
-	log.Println(http.ListenAndServe(":8080", mux))
-
-	c.Close()
+	//c := gortsplib.Client{}
+	//
+	//// parse URL
+	//u, err := base.ParseURL("rtsp://192.168.1.20:8554/unicast")
+	//if err != nil {
+	//	log.Fatalf("Error parsing URL: %v", err)
+	//}
+	//
+	//// connect to the server
+	//err = c.Start(u.Scheme, u.Host)
+	//if err != nil {
+	//	log.Fatalf("Error connecting to server: %v", err)
+	//}
+	//defer c.Close()
+	//
+	//// find available medias
+	//desc, _, err := c.Describe(u)
+	//if err != nil {
+	//	log.Fatalf("Error describing stream: %v", err)
+	//}
+	//
+	//// find the H264 media and format
+	//var forma *format.H264
+	//medi := desc.FindFormat(&forma)
+	//if medi == nil {
+	//	log.Fatalf("H264 media not found")
+	//}
+	//
+	//// setup RTP/H264 -> H264 decoder
+	//rtpDec, err := forma.CreateDecoder()
+	//if err != nil {
+	//	log.Fatalf("Error creating decoder: %v", err)
+	//}
+	//
+	//// setup H264 -> raw frames decoder
+	//frameDec := &h264Decoder{}
+	//err = frameDec.initialize()
+	//if err != nil {
+	//	log.Fatalf("Error creating decoder: %v", err)
+	//}
+	//defer frameDec.close()
+	//
+	//// if SPS and PPS are present into the SDP, send them to the decoder
+	//if forma.SPS != nil {
+	//	frameDec.decode(forma.SPS)
+	//}
+	//if forma.PPS != nil {
+	//	frameDec.decode(forma.PPS)
+	//}
+	//
+	//// setup a single media
+	//_, err = c.Setup(desc.BaseURL, medi, 0, 0)
+	//if err != nil {
+	//	log.Fatalf("Error setting up stream: %v", err)
+	//}
+	//
+	//imgFrames := make(chan image.Image)
+	//
+	//detectedImg := make(chan image.Image)
+	//
+	//processingImage := atomic.Bool{}
+	//
+	//lastTime := time.Now()
+	//fps := 0.0
+	//
+	//// called when a RTP packet arrives
+	//c.OnPacketRTP(medi, forma, func(pkt *rtp.Packet) {
+	//	// decode timestamp
+	//	_, ok := c.PacketPTS(medi, pkt)
+	//	if !ok {
+	//		//log.Printf("waiting for timestamp")
+	//		return
+	//	}
+	//
+	//	// extract access units from RTP packets
+	//	au, err := rtpDec.Decode(pkt)
+	//	if err != nil {
+	//		if err != rtph264.ErrNonStartingPacketAndNoPrevious && err != rtph264.ErrMorePacketsNeeded {
+	//			log.Printf("ERR: %v", err)
+	//		}
+	//		return
+	//	}
+	//
+	//	for _, nalu := range au {
+	//		// convert NALUs into RGBA frames
+	//		img, err := frameDec.decode(nalu)
+	//		if err != nil {
+	//			log.Fatalf("Error decoding frame: %v", err)
+	//		}
+	//
+	//		// wait for a frame
+	//		if img == nil {
+	//			continue
+	//		}
+	//
+	//		imgFrames <- img
+	//
+	//		fps = 1 / time.Since(lastTime).Seconds()
+	//		lastTime = time.Now()
+	//
+	//		if !processingImage.Load() {
+	//			processingImage.Store(true)
+	//			detectedImg <- img
+	//		}
+	//
+	//		//log.Printf("decoded frame with PTS %v and size %v", pts, img.Bounds().Max)
+	//	}
+	//})
+	//
+	//// start playing
+	//_, err = c.Play(nil)
+	//if err != nil {
+	//	log.Fatalf("Error opening stream: %v", err)
+	//}
+	//
+	//latestDetections := []*Detection{}
+	//
+	//go func() {
+	//	for {
+	//		img := <-detectedImg
+	//
+	//		t1 := time.Now()
+	//
+	//		detections, detErr := detect_objects_on_image(img)
+	//		if detErr != nil {
+	//			log.Printf("error detecting objects: %v", detErr)
+	//		}
+	//
+	//		for _, detection := range detections {
+	//			log.Printf("detected: %+v", detection)
+	//		}
+	//
+	//		latestDetections = detections
+	//
+	//		detectionFps := 1 / time.Since(t1).Seconds()
+	//
+	//		log.Printf("stream fps: %.1f, detection fps: %.1f", fps, detectionFps)
+	//
+	//		processingImage.Store(false)
+	//	}
+	//}()
+	//
+	//stream := mjpeg.Handler{
+	//	Next: func() (image.Image, error) {
+	//		img := <-imgFrames
+	//
+	//		// draw bounding boxes
+	//		for _, detection := range latestDetections {
+	//			// draw rectangle
+	//			drawHollowRectangle(
+	//				img.(*image.RGBA),
+	//				int(detection.X1),
+	//				int(detection.Y1),
+	//				int(detection.X2-detection.X1),
+	//				int(detection.Y2-detection.Y1),
+	//				2,
+	//			)
+	//
+	//			// add label
+	//			addLabel(
+	//				img.(*image.RGBA),
+	//				int(detection.X1),
+	//				int(detection.Y1),
+	//				fmt.Sprintf("%s %.2f", detection.Label, detection.Probability),
+	//			)
+	//		}
+	//
+	//		return img, nil
+	//	},
+	//	Options: &jpeg.Options{Quality: 80},
+	//}
+	//
+	//mux := http.NewServeMux()
+	//mux.Handle("/stream", stream)
+	//
+	//log.Println(http.ListenAndServe(":8080", mux))
+	//
+	//c.Close()
 }
 
 func drawHollowRectangle(img *image.RGBA, x, y, width, height, borderWidth int) {
